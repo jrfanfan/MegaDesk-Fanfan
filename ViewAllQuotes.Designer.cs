14e82eb258e6ae9bb2a668cc6d2e9b4bac74097f
Jrfanfan <jrfanfan@yahoo.fr>
2025-05-22 18:47:10 -0400

    upgrade

--- ViewAllQuotes.Designer.cs
+++ ViewAllQuotes.Designer.cs
@@ -28,18 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(0, 0);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.Size = new Size(1120, 576);
+            dataGridView1.TabIndex = 0;
             // 
             // ViewAllQuotes
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1120, 576);
+            Controls.Add(dataGridView1);
             Name = "ViewAllQuotes";
             Text = "ViewAllQuotes";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private DataGridView dataGridView1;
     }
 }
